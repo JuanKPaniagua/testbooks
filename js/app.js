863eed5a8022f8a482722dfb3400591bd4a4049b
JuanKPaniagua <paniaguacortes214@hotmail.com>
2020-11-03 15:42:44 -0600

    testing

--- js/app.js
+++ js/app.js
@@ -8,27 +8,27 @@ function getAll() {
 }
 
 function checkId(item) {
-    return item._id==this
+	return item._id==this
 }
 
 function getById(query) {
   fetch('/template/detail.html')
     .then((response) => response.text())
     .then((template) => {
-      var params = new URLSearchParams(query);
-      var elem = data.find(checkId,params.get('id'));
+	  var params = new URLSearchParams(query);
+	  var elem = data.find(checkId,params.get('id'));
       var rendered = Mustache.render(template, elem);
       document.getElementById('content').innerHTML = rendered;    
    });
 }
 
 function init() {
-    router = new Navigo(null, false, '#!');
-    router.on({
-      '/get': function(_,query) {
-         getById(query);
-      }
-    });
-    router.on(() => getAll());
-    router.resolve();
+	router = new Navigo(null, false, '#!');
+	router.on({
+	  '/get': function(_,query) {
+		 getById(query);
+	  }
+	});
+	router.on(() => getAll());
+	router.resolve();
 }
